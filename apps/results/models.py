216d17bbb044f7dc6ee92b3702927387339a1ce3
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-04-22 18:33:39 +0200

    Added model representation.

--- apps/results/models.py
+++ apps/results/models.py
@@ -7,3 +7,10 @@ from django.db import models
 class TestResult(models.Model):
     id = models.AutoField(primary_key=True)
     test = models.ForeignKey('tests.Test', related_name='results')
+
+    def __repr__(self):
+        return "{0} #{1}".format(self.__class__.__name__, self.id)
+
+    __unicode__ = __repr__
+
+    __str__ = __repr__
--- apps/tests/models.py
+++ apps/tests/models.py
@@ -18,3 +18,16 @@ class Test(models.Model):
     main_revision = models.CharField(max_length=10)
     secondary_revision = models.CharField(max_length=10)
     status = enum.EnumField(TestStatus, default=TestStatus.PENDING)
+
+    def __repr__(self):
+        return "{0}(#{1}): {2}@{3}".format(
+            self.__class__.__name__,
+            self.id,
+            self.test_uri,
+            self.main_revision
+        )
+
+    def __unicode__(self):
+        return "{0} #{1}".format(self.__class__.__name__, self.id)
+
+    __str__ = __unicode__
