5e4011652eb4ef421ac72410805961a84f54f97a
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-04-22 18:20:03 +0200

    Added Test.status field.

--- apps/tests/models.py
+++ apps/tests/models.py
@@ -2,6 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django_enumfield import enum
+
+
+class TestStatus(enum.Enum):
+    PENDING = 0
+    IN_PROGRESS = 1
+    DONE = 2
+    ERROR = -1
 
 
 class Test(models.Model):
@@ -9,3 +17,4 @@ class Test(models.Model):
     test_uri = models.URLField()
     main_revision = models.CharField(max_length=10)
     secondary_revision = models.CharField(max_length=10)
+    status = enum.EnumField(TestStatus, default=TestStatus.PENDING)
