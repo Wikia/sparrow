3640528c69f32f398a9532ac4c2162c9e20f4d57
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-04-27 18:09:34 +0200

    More complementary test cases for TestResult API.
    More to come.

--- tests/tests_results.py
+++ tests/tests_results.py
@@ -11,6 +11,7 @@ from rest_framework.test import APITestCase
 class TestResultTestCase(APITestCase):
     def setUp(self):
         self.test_run = mommy.make('test_runs.TestRun')
+        self.task_to_delete = mommy.make('results.TestResult')
 
     def test_create_result(self):
         url = reverse('testresult-list')
@@ -21,3 +22,23 @@ class TestResultTestCase(APITestCase):
 
         response = self.client.post(url, payload)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_read_result(self):
+        url = reverse('testresult-detail', args=[self.task_to_delete.id, ])
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_result(self):
+        url = reverse('testresult-detail', args=[self.task_to_delete.id, ])
+        payload = {'results': {'foo': 'bar', }}
+
+        response = self.client.patch(url, payload)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['results'], payload['results'])
+
+    def test_delete_result(self):
+        url = reverse('testresult-detail', args=[self.task_to_delete.id, ])
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
