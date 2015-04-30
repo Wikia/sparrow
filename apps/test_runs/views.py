90b1f08446d20271d5217233664f67b6c3c68e6a
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-04-30 12:18:32 +0200

    Automatic Task creation when creating TestRun.

--- apps/test_runs/views.py
+++ apps/test_runs/views.py
@@ -2,11 +2,29 @@
 from __future__ import unicode_literals
 
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import TestRunSerializer
 from .models import TestRun
+from .models import TestRunStatus
+from tasks.models import Task
 
 
 class TestRunViewSet(viewsets.ModelViewSet):
     queryset = TestRun.objects.all()
     serializer_class = TestRunSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        if serializer.instance.status == TestRunStatus.PENDING:
+            # create related object
+            task = Task()
+            task.test_run_id = serializer.instance.id
+            task.save()
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
