6354a652bd12d8469a995afe7d39bca9699c01f4
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-05-11 12:55:39 +0200

    Added tasks/#/lock to enable returning locked tasks to queue.

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -3,8 +3,11 @@ from __future__ import unicode_literals
 
 from rest_framework import viewsets
 from rest_framework.decorators import list_route
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotAcceptable
 
 from .serializers import TaskSerializer
 from .models import Task, TaskStatus
@@ -15,6 +18,27 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    @detail_route(methods=['delete'])
+    def lock(self, request, pk):
+        """ This method implements releasing lock on task in progress.
+
+        Queue clients needs to be able to return tasks to queue (retry process) on non fatal errors. This method
+        sets status to IN_PROGRESS and given task will be processed in the earliest fetch possible.
+
+        Args:
+            request (Request): Request data
+            pk (int): Id of the task to remove lock from
+        """
+        task = self.get_object()
+
+        if task.status != TaskStatus.IN_PROGRESS:
+            raise NotAcceptable('Task not IN PROGRESS status!')
+
+        task.status = TaskStatus.PENDING
+        task.save()
+
+        return Response(status=status.HTTP_202_ACCEPTED)
 
     @list_route()
     def fetch(self, request):
