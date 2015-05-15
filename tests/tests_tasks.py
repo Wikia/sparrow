9450f2eae9ee68f596847a52f2090391dfeb5fbe
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-05-15 21:56:37 +0200

    Added test for Task fetch().

--- tests/tests_tasks.py
+++ tests/tests_tasks.py
@@ -7,6 +7,7 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from tasks.models import Task
 from tasks.models import TaskStatus
 
 
@@ -33,6 +34,22 @@ class TestResultTestCase(APITestCase):
 
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK, msg='Read failed: {0}'.format(response.data))
+
+    def test_fetch_task(self):
+        url = reverse('task-fetch')
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg='Fetch failed: {0}'.format(response.data))
+        self.assertEqual(response.data['status'], TaskStatus.IN_PROGRESS)
+
+        # set all tasks from PENDING to ERROR to test if fetch returns 404
+        for task in Task.objects.filter(status=TaskStatus.PENDING):
+            task.status = TaskStatus.ERROR
+            task.save()
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND,
+                         msg='Fetch none failed: {0}'.format(response.data))
 
     def test_update_result(self):
         url = reverse('task-detail', args=[self.task_to_delete.id, ])
