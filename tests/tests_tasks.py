5e78c03f1b19b81065bdff1ba1ac661e61154e01
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-05-15 22:17:50 +0200

    Added test for lock delete on Task.

--- tests/tests_tasks.py
+++ tests/tests_tasks.py
@@ -42,6 +42,33 @@ class TestResultTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK, msg='Fetch failed: {0}'.format(response.data))
         self.assertEqual(response.data['status'], TaskStatus.IN_PROGRESS)
 
+        # now to remove the lock from fetched Task
+        lock_url = reverse('task-lock', args=[response.data['id']])
+        response = self.client.delete(lock_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg='Lock remove failed: {0}'.format(response.data))
+        # we expect status to be set to ERROR since no results were generated for this Task
+        self.assertEqual(response.data['status'], TaskStatus.ERROR)
+
+        # now we create task with some results
+        proper_task = mommy.make('tasks.Task')
+        mommy.make('results.TestResult', task=proper_task)
+
+        # now we need to fetch it
+        url = reverse('task-fetch')
+        while True:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, status.HTTP_200_OK,
+                             msg='Cannot fetch proper task: {0}'.format(response.data))
+            if response.data['id'] == proper_task.id:
+                break
+
+        lock_url = reverse('task-lock', args=[proper_task.id])
+        response = self.client.delete(lock_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         msg='Lock remove failed for proper task: {0}'.format(response.data))
+        # in this we expect status to be set to DONE - we do have Result associated with this Task
+        self.assertEqual(response.data['status'], TaskStatus.DONE)
+
         # set all tasks from PENDING to ERROR to test if fetch returns 404
         for task in Task.objects.filter(status=TaskStatus.PENDING):
             task.status = TaskStatus.ERROR
