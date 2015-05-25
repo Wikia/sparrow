163e621012bb5beb44e7cb9cb1f0866920b1b975
Łukasz Harasimowicz <lukasz.harasimowicz@wikia-inc.com>
2015-05-25 17:44:03 +0200

    Added raw results being saved when collecting 'backend' metrics.

--- apps/results/serializers.py
+++ apps/results/serializers.py
@@ -10,14 +10,14 @@ from common.field_serializers import JSONField
 
 class TestResultSerializer(serializers.HyperlinkedModelSerializer):
     results = JSONField(required=False)
-    raw_results = serializers.PrimaryKeyRelatedField(many=True, read_only=True, required=False)
+    raw_results = serializers.HyperlinkedIdentityField(many=True, read_only=True, required=False, view_name='testrawresult-detail')
 
     class Meta:
         model = TestResult
         fields = ('id', 'test_run', 'task', 'created', 'results', 'raw_results', 'url')
 
 class TestRawResultSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name='testrawresult-detail')
+    data = JSONField(required=False)
 
     class Meta:
         model = TestRawResult
