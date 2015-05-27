bed7ace34b292feacce54d9e4ab3c841cf537539
Władysław Bodzek <wladek@wikia-inc.com>
2015-05-27 19:21:38 +0200

    Add --runs argument to testrunner_simple_test

--- apps/testrunner/management/commands/testrunner_simple_test.py
+++ apps/testrunner/management/commands/testrunner_simple_test.py
@@ -10,16 +10,18 @@ from ...runner import Task
 class Command(BaseCommand):
     help = 'Run fake test'
 
-    # def add_arguments(self, parser):
-    #     parser.add_argument('test_id', nargs='+', type=int)
+    def add_arguments(self, parser):
+        parser.add_argument('--runs', type=int, default=2)
 
     def handle(self, *args, **options):
+        num_runs = options['runs']
+        print "Spawning SimpleTestSuite with {} runs...".format(num_runs)
         task = Task(None,{
             'id': 1,
             'app_commit': 'dev',
             'config_commit': 'dev',
             'url': 'http://muppet.synth1.wikia-dev.com/wiki/Special:Version',
-            'retries': 2
+            'retries': num_runs
         })
         test_run = SimpleTestSuite(task)
         test_run.run()
