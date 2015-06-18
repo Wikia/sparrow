9996a7e5ad1b40e6a3f10e1f91e36a50ae8b3b72
Władysław Bodzek <wladek@wikia-inc.com>
2015-06-18 20:19:24 +0200

    Create smart endpoint to handle compare requests

--- apps/compare_requests/serializers.py
+++ apps/compare_requests/serializers.py
@@ -7,8 +7,13 @@ from .models import CompareRequest
 
 
 class CompareRequestSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = CompareRequest
-        fields = ('id', 'repo', 'pull_request_num', 'base_ref', 'base_sha', 'base_test_run', 'head_ref', 'head_sha', 'head_test_run')
+        fields = ('id', 'repo', 'pull_request_num', 'base_ref', 'base_sha', 'base_test_run', 'head_ref', 'head_sha',
+                  'head_test_run')
 
+
+class CompareRequestCreateSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CompareRequest
+        fields = ('repo', 'pull_request_num')
