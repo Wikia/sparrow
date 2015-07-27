08d95a4c7eac668040c58d5344f0365997bdd437
Władysław Bodzek <wladek@wikia-inc.com>
2015-07-27 13:39:10 +0200

    Auto merge pull request branch with dev on compare request

--- apps/compare_requests/views.py
+++ apps/compare_requests/views.py
@@ -13,6 +13,7 @@ from test_runs.models import TestRun
 
 from common.github_integration import GitHub
 
+APP_TARGET_BRANCH = 'dev'
 CONFIG_REPO = 'Wikia/config'
 CONFIG_BRANCH = 'dev'
 COMPARE_URL = 'http://muppet.synth1.wikia-dev.com/wiki/Kermit'
@@ -74,20 +75,16 @@ class CompareRequestViewSet(viewsets.ModelViewSet):
         pull_info = github.get_pull_request_info(repo_name, pull_req_num)
         logger.debug('  received: {}'.format(ujson.dumps(pull_info)))
 
-        app_head_info = pull_info['head']
+        app_branch_info = pull_info['head']
         logger.debug('Querying branch info: {} : {}'.format(repo_name, pull_info['base']['ref']))
-        app_base_info = github.get_branch_info(repo_name, pull_info['base']['ref'])['head']
-        logger.debug('  received: {}'.format(ujson.dumps(app_head_info)))
-
-        logger.debug('Querying branch info: {} : {}'.format(CONFIG_REPO, CONFIG_BRANCH))
-        config_info = github.get_branch_info(CONFIG_REPO, CONFIG_BRANCH)['head']
-        logger.debug('  received: {}'.format(ujson.dumps(config_info)))
+        app_dev_info = github.get_branch_info(repo_name, APP_TARGET_BRANCH)['head']
+        logger.debug('  received: {}'.format(ujson.dumps(app_branch_info)))
 
         logger.debug('Searching for existing compare requests for: head_sha={} base_sha={}'.format(
-            app_head_info['sha'], app_base_info['sha']))
+            app_branch_info['sha'], app_dev_info['sha']))
         existing_requests = CompareRequest.objects.filter(
-            head_sha=app_head_info['sha'],
-            base_sha=app_base_info['sha']
+            head_sha=app_branch_info['sha'],
+            base_sha=app_dev_info['sha']
         ).order_by('-created')[0:1]
 
         if len(existing_requests) == 1:
@@ -97,19 +94,35 @@ class CompareRequestViewSet(viewsets.ModelViewSet):
         else:
             logger.debug('  not found, creating new one...')
 
+            app_merged_branch_name = 'sparrow-test-{}-{}'.format(app_branch_info['sha'], app_dev_info['sha'])
+            logger.debug('Creating merged branch: {}'.format(app_merged_branch_name))
+            try:
+                app_merged_info = github.create_merged_ref(repo_name, app_merged_branch_name, app_branch_info['sha'],
+                                                              APP_TARGET_BRANCH)
+            except:
+                # assume the branch has been correctly created by another process
+                app_merged_info = github.get_branch_info(repo_name, app_merged_branch_name)
+
+            app_merged_info = app_merged_info['head']
+            logger.debug('  received: {}'.format(ujson.dumps(app_merged_info)))
+
+            logger.debug('Querying branch info: {} : {}'.format(CONFIG_REPO, CONFIG_BRANCH))
+            config_dev_info = github.get_branch_info(CONFIG_REPO, CONFIG_BRANCH)['head']
+            logger.debug('  received: {}'.format(ujson.dumps(config_dev_info)))
+
             compare_request = CompareRequest()
             compare_request.repo = repo_name
             compare_request.pull_request_num = pull_req_num
 
-            compare_request.head_ref = app_head_info['ref']
-            compare_request.head_sha = app_head_info['sha']
-            compare_request.head_test_run = create_test_run(COMPARE_URL, app_head_info['ref'],
-                                                            config_info['ref'])
+            compare_request.head_ref = app_branch_info['ref']
+            compare_request.head_sha = app_merged_info['sha']
+            compare_request.head_test_run = create_test_run(COMPARE_URL, app_branch_info['ref'],
+                                                            config_dev_info['ref'])
 
-            compare_request.base_ref = app_base_info['ref']
-            compare_request.base_sha = app_base_info['sha']
-            compare_request.base_test_run = create_test_run(COMPARE_URL, app_base_info['ref'],
-                                                            config_info['ref'])
+            compare_request.base_ref = app_dev_info['ref']
+            compare_request.base_sha = app_dev_info['sha']
+            compare_request.base_test_run = create_test_run(COMPARE_URL, app_dev_info['ref'],
+                                                            config_dev_info['ref'])
 
             compare_request.save()
             logger.debug('Created CompareRequest.id = {}'.format(compare_request.id))
--- common/github_integration.py
+++ common/github_integration.py
@@ -6,9 +6,15 @@ import github
 class GitHub(object):
     def __init__(self):
         self.github = github.Github(settings.GITHUB_TOKEN)
+        self.repo_cache = {}
+
+    def get_repo(self, repo_name):
+        if not repo_name in self.repo_cache:
+            self.repo_cache[repo_name] = self.github.get_repo(repo_name)
+        return self.repo_cache[repo_name]
 
     def get_branch_info(self, repo_name, branch_name):
-        repo = self.github.get_repo(repo_name)
+        repo = self.get_repo(repo_name)
         branch = repo.get_branch(branch_name)
         return {
             'repo': repo_name,
@@ -19,7 +25,7 @@ class GitHub(object):
         }
 
     def get_pull_request_info(self, repo_name, pull_num):
-        pull_req = self.github.get_repo(repo_name).get_pull(pull_num)
+        pull_req = self.get_repo(repo_name).get_pull(pull_num)
         return {
             'repo': repo_name,
             'pull_num': pull_num,
@@ -34,5 +40,21 @@ class GitHub(object):
         }
 
     def post_pull_request_comment(self, repo_name, pull_num, comment_body):
-        pull_req = self.github.get_repo(repo_name).get_pull(pull_num)
+        pull_req = self.get_repo(repo_name).get_pull(pull_num)
         pull_req.create_issue_comment(comment_body)
+
+    def create_merged_ref(self, repo_name, target_ref_name, *shas):
+        target_ref_full_name = 'refs/heads/{}'.format(target_ref_name)
+        repo = self.get_repo(repo_name)
+        target_ref = repo.create_git_ref(target_ref_full_name, shas[0])
+
+        for i in range(1, len(shas)):
+            merge_commit = repo.merge(target_ref, shas[i], 'Sparrow Code Setup: Merging "{}"'.format(shas[i]))
+
+        return {
+            'repo': repo_name,
+            'head': {
+                'ref': target_ref.ref,
+                'sha': merge_commit.sha
+            }
+        }
